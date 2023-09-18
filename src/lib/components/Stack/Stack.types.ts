2b670583298e849da58e5b160ef535abc8e7f2ae
mishamyrt <misha@myrt.co>
2023-09-19 02:01:01 +0300

    feat(Stack): Add missing xs spacing

--- src/lib/components/Stack/Stack.types.ts
+++ src/lib/components/Stack/Stack.types.ts
@@ -3,7 +3,7 @@ type StackAlign = 'start' | 'end' | 'center' | 'normal'
 export interface StackProps {
   align: StackAlign
   justify: 'space-between' | 'space-around' | StackAlign
-  gap: 's' | 'm' | 'l'
+  gap: 's' | 'm' | 'l' | 'xs'
   direction: 'vertical' | 'horizontal'
   maxWidth: number
 }
