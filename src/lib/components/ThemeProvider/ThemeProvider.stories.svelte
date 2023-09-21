eeda49e38b982cd99a10224e10c486260d546f52
Ekaterina Makridi <makridi.ekaterina@gmail.com>
2023-09-21 23:45:51 +0300

    docs(ThemeProvider): Add documentation of theme mixins

--- src/lib/components/ThemeProvider/ThemeProvider.stories.svelte
+++ src/lib/components/ThemeProvider/ThemeProvider.stories.svelte
@@ -29,3 +29,37 @@
 </Template>
 
 <Story template="base" name="Basic" />
+
+<Story name="Mixins">
+  <div class="theme-mixins-example">—</div>
+</Story>
+
+<style lang="scss">
+  .theme-mixins-example {
+    color: var(--color-content-primary);
+  }
+
+  @include mac('.theme-mixins-example') {
+    &::before {
+      content: 'macOS';
+    }
+  }
+
+  @include linux('.theme-mixins-example') {
+    &::before {
+      content: 'Linux';
+    }
+  }
+
+  @include dark {
+    .theme-mixins-example {
+      &::after {
+        content: 'Dark';
+      }
+    }
+  }
+
+  @include light('.theme-mixins-example::after') {
+    content: 'Light';
+  }
+</style>
