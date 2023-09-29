50e35eb62b4d3b671c0ca65aea4d56f4a37720de
mishamyrt <misha@myrt.co>
2023-09-29 09:47:18 +0300

    feat(Typography): introduce missing `caption-l`

--- src/lib/components/Typography/Typography.types.ts
+++ src/lib/components/Typography/Typography.types.ts
@@ -1,12 +1,12 @@
 type TypographyVariants =
   | 'caption-s'
   | 'caption-m'
+  | 'caption-l'
   | 'text-s'
   | 'text-m'
   | 'text-l'
   | 'heading-s'
   | 'heading-m'
-  | 'caption-m'
 type TypographyColor = 'primary' | 'tertiary'
 type TypographyAlign = 'left' | 'right' | 'center'
 
