395035c49ebb008a0c030d97144f67ad636c8c8a
mishamyrt <misha@myrt.co>
2023-10-01 21:00:28 +0300

    docs(getUniqueId): add stories and docs

---
+++ src/lib/utils/getUniqueId/getUniqueId.stories.svelte
@@ -0,0 +1,33 @@
+<script context="module">
+  import { getUniqueId } from './getUniqueId.js'
+  export const meta = {
+    title: 'Utilities/getUniqueId',
+    component: getUniqueId,
+  }
+</script>
+
+<script lang="ts">
+  import { Story } from '@storybook/addon-svelte-csf'
+
+  import { Stack, Typography } from '$lib/index.js'
+
+  const prefix = 'prefix-'
+  const indexes = new Array(10).fill(null)
+</script>
+
+<Story name="Basic">
+  <Stack direction="vertical">
+    {#each indexes as _}
+      <Typography>{getUniqueId()}</Typography>
+    {/each}
+  </Stack>
+</Story>
+
+<Story name="Prefix">
+  <Stack direction="vertical">
+    <Typography variant="heading-s">Prefix: '{prefix}'</Typography>
+    {#each indexes as _}
+      <Typography>{getUniqueId(prefix)}</Typography>
+    {/each}
+  </Stack>
+</Story>
