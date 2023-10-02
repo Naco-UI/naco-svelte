9cf3c750242cfb4a2f495e8b53da23c83efac31d
mishamyrt <misha@myrt.co>
2023-10-02 21:46:44 +0300

    refactor(docs): improve mdx imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import { resolve } from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [sveltekit()],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
+  },
+  resolve: {
+    alias: {
+      '$storybook/docs': `${resolve(__dirname, '.storybook/components/docs')}`,
+    },
   },
 })
