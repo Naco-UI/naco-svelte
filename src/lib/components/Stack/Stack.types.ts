220f5b7a8c0aa7d1ee6fd00ab941099bf195ab88
mishamyrt <misha@myrt.co>
2023-10-17 11:39:42 +0300

    fix(Stack): re-export types, add missing `sl` gap

--- src/lib/components/Stack/Stack.types.ts
+++ src/lib/components/Stack/Stack.types.ts
@@ -1,7 +1,7 @@
-type StackAlign = 'start' | 'end' | 'center' | 'normal'
-type StackJustify = 'space-between' | 'space-around' | StackAlign
-type StackGap = 's' | 'm' | 'l' | 'xs' | 'xxs' | 'none'
-type StackDirection = 'vertical' | 'horizontal'
+export type StackAlign = 'start' | 'end' | 'center' | 'normal'
+export type StackJustify = 'space-between' | 'space-around' | StackAlign
+export type StackGap = 's' | 'sl' | 'm' | 'l' | 'xs' | 'xxs' | 'none'
+export type StackDirection = 'vertical' | 'horizontal'
 
 export interface StackProps {
   align?: StackAlign
