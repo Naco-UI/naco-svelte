9cb19895975c7cf15d8a704271a8f37a006187c7
mishamyrt <misha@myrt.co>
2024-01-16 01:03:39 +0300

    refactor(portal): better split code, return first update results

--- src/lib/actions/portal/portal.types.ts
+++ src/lib/actions/portal/portal.types.ts
@@ -3,4 +3,5 @@ export type PortalTarget = HTMLElement | string
 export interface PortalHooks {
   update: (target: PortalTarget) => Promise<void>
   destroy: () => void
+  attached: Promise<void>
 }
