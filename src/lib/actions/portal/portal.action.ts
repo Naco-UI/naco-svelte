dc564895edc62a55e6a6a769a4ec8e01e1cdebe9
mishamyrt <misha@myrt.co>
2024-01-16 01:03:39 +0300

    refactor(portal): better split code, return first update results

--- src/lib/actions/portal/portal.action.ts
+++ src/lib/actions/portal/portal.action.ts
@@ -2,35 +2,22 @@ import { tick } from 'svelte'
 
 import type { PortalHooks, PortalTarget } from './portal.types.js'
 
-function formatTypeError(target: any): string {
-  const targetType = target === null ? 'null' : typeof target
-  return `Unknown portal target type: ${targetType}.
-Allowed types: string (CSS selector) or HTMLElement.`
-}
-
 /**
  * Places element to target
  * Usage: <div use:portal={'css selector'}> or <div use:portal={document.body}>
  */
 export function portal(el: HTMLElement, target: PortalTarget = 'body'): PortalHooks {
-  let targetEl
-  async function update(newTarget: PortalTarget): Promise<void> {
-    target = newTarget
-    if (typeof target === 'string') {
-      targetEl = document.querySelector(target)
-      if (targetEl === null) {
-        await tick()
-        targetEl = document.querySelector(target)
-      }
-      if (targetEl === null) {
-        throw new Error(`No element found matching css selector: "${target}"`)
-      }
-    } else if (target instanceof HTMLElement) {
-      targetEl = target
-    } else {
-      throw new TypeError(formatTypeError(target))
+  let lastTarget: HTMLElement
+  async function update(t: PortalTarget): Promise<void> {
+    await tick()
+    const targetElement = getTargetElement(t)
+    if (!lastTarget) {
+      lastTarget = targetElement
+    } else if (lastTarget !== targetElement) {
+      lastTarget.removeChild(el)
     }
-    targetEl.appendChild(el)
+    targetElement.appendChild(el)
+    lastTarget = targetElement
     el.hidden = false
   }
 
@@ -40,9 +27,37 @@ export function portal(el: HTMLElement, target: PortalTarget = 'body'): PortalHo
     }
   }
 
-  update(target)
+  const attached = update(target)
   return {
     update,
     destroy,
+    attached,
   }
+}
+
+export class TargetTypeError extends TypeError {
+  constructor(target: any) {
+    const targetType = target === null ? 'null' : typeof target
+    super(`Unknown portal target type: ${targetType}.
+    Allowed types: string (CSS selector) or HTMLElement.`)
+  }
+}
+
+export class ElementNotFoundError extends Error {
+  constructor(selector: string) {
+    super(`No element found matching css selector: "${selector}"`)
+  }
+}
+
+function getTargetElement(t: PortalTarget): HTMLElement {
+  if (typeof t === 'string') {
+    const element = document.querySelector<HTMLElement>(t)
+    if (element === null) {
+      throw new ElementNotFoundError(t)
+    }
+    return element
+  } else if (t instanceof HTMLElement) {
+    return t
+  }
+  throw new TargetTypeError(t)
 }
