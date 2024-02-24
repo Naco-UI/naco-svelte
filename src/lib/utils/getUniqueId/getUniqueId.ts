62a8f2700b4350f53bc5ddb7a8b3a900b26b3a2d
mishamyrt <misha@myrt.co>
2024-02-24 20:17:20 +0300

    docs(getUniqueId): add minimal jsdoc

--- src/lib/utils/getUniqueId/getUniqueId.ts
+++ src/lib/utils/getUniqueId/getUniqueId.ts
@@ -1,5 +1,8 @@
 let uniqueId = 0
 
+/**
+ * Generates a unique identifier with an optional prefix.
+ */
 export function getUniqueId(prefix = ''): string {
   uniqueId += 1
   return `${prefix}${uniqueId}`
