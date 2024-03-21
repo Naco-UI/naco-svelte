512515320ad72ae015165e656c1b49a914a591d9
mishamyrt <misha@myrt.co>
2024-03-21 18:53:57 +0300

    chore: migrate to Storybook 8

--- .storybook/components/docs/Props.ts
+++ .storybook/components/docs/Props.ts
@@ -1,3 +1,3 @@
-import { ArgsTable as Props, PRIMARY_STORY } from '@storybook/addon-docs'
-Props.defaultProps = { ...Props.defaultProps, story: PRIMARY_STORY }
+import { Controls as Props, PRIMARY_STORY } from '@storybook/addon-docs'
+// Props.defaultProps = { ...Props.defaultProps }
 export { Props }
